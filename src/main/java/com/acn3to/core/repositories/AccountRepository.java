8ff53c16f0aea4d716bab2727488b281ba07cbef
acn3to <costanetoow@gmail.com>
2024-07-23 16:04:48 -0300

    fix(repositories): correct date handling and format in BankAgencyRepository and TransactionRepository

--- src/main/java/com/acn3to/core/repositories/AccountRepository.java
+++ src/main/java/com/acn3to/core/repositories/AccountRepository.java
@@ -1,102 +1,62 @@
 package com.acn3to.core.repositories;
 
 import com.acn3to.core.entities.Account;
-import com.google.gson.JsonObject;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.ReplaceOptions;
 import org.bson.Document;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
-/**
- * Repository class for managing accounts in a MongoDB collection.
- * <p>
- * This class provides methods for saving, finding, and deleting accounts in the MongoDB collection.
- * </p>
- */
+import static com.mongodb.client.model.Filters.eq;
+
 public class AccountRepository {
-    private final MongoCollection<Document> collection;
+    private final MongoCollection<Document> accountsCollection;
 
-    /**
-     * Constructs an AccountRepository with a MongoDB connection.
-     *
-     * @param database the MongoDatabase instance to use
-     */
     public AccountRepository(MongoDatabase database) {
-        this.collection = database.getCollection("accounts");
+        this.accountsCollection = database.getCollection("accounts");
     }
 
-    /**
-     * Saves an account to the MongoDB collection.
-     *
-     * @param account the Account object to save
-     */
     public void save(Account account) {
-        Document query = new Document("accountId", account.getAccountId());
-        Document doc = Document.parse(accountToJson(account).toString());
-        collection.replaceOne(query, doc, new ReplaceOptions().upsert(true));
+        Document document = new Document("accountId", account.getAccountId())
+                .append("accountHolderName", account.getAccountHolderName())
+                .append("accountType", account.getAccountType())
+                .append("accountStatus", account.getAccountStatus())
+                .append("creationDate", account.getCreationDate())
+                .append("lastUpdatedDate", account.getLastUpdatedDate())
+                .append("balance", account.getBalance());
+        accountsCollection.replaceOne(eq("accountId", account.getAccountId()), document, new ReplaceOptions().upsert(true));
     }
 
-    /**
-     * Finds an account by its ID.
-     *
-     * @param accountId the ID of the account to find
-     * @return the Account object with the given ID, or null if not found
-     */
     public Account findById(int accountId) {
-        Document query = new Document("accountId", accountId);
-        Document doc = collection.find(query).first();
-        return doc != null ? documentToAccount(doc) : null;
+        Document document = accountsCollection.find(eq("accountId", accountId)).first();
+        if (document == null) {
+            return null;
+        }
+        return new Account(
+                document.getDouble("balance"),
+                document.getInteger("accountId"),
+                document.getString("accountHolderName"),
+                document.getString("accountType"),
+                document.getString("accountStatus"),
+                document.getDate("creationDate")
+        );
     }
 
-    /**
-     * Finds all accounts in the MongoDB collection.
-     *
-     * @return a list of all Account objects
-     */
     public List<Account> findAll() {
         List<Account> accounts = new ArrayList<>();
-        for (Document doc : collection.find()) {
-            accounts.add(documentToAccount(doc));
+        for (Document doc : accountsCollection.find()) {
+            Account account = new Account(
+                    doc.getDouble("balance"),
+                    doc.getInteger("accountId"),
+                    doc.getString("accountHolderName"),
+                    doc.getString("accountType"),
+                    doc.getString("accountStatus"),
+                    doc.getDate("creationDate")
+            );
+            accounts.add(account);
         }
         return accounts;
-    }
-
-    /**
-     * Converts an Account object to a JsonObject.
-     *
-     * @param account the Account object
-     * @return the JsonObject representation of the Account
-     */
-    private JsonObject accountToJson(Account account) {
-        JsonObject json = new JsonObject();
-        json.addProperty("accountId", account.getAccountId());
-        json.addProperty("accountHolderName", account.getAccountHolderName());
-        json.addProperty("accountType", account.getAccountType());
-        json.addProperty("accountStatus", account.getAccountStatus());
-        json.addProperty("creationDate", account.getCreationDate().toInstant().toString());
-        json.addProperty("lastUpdatedDate", account.getLastUpdatedDate().toInstant().toString());
-        json.addProperty("balance", account.getBalance());
-        return json;
-    }
-
-    /**
-     * Converts a Document from MongoDB to an Account object.
-     *
-     * @param doc the MongoDB Document
-     * @return the Account object
-     */
-    private Account documentToAccount(Document doc) {
-        int accountId = doc.getInteger("accountId");
-        String accountHolderName = doc.getString("accountHolderName");
-        String accountType = doc.getString("accountType");
-        String accountStatus = doc.getString("accountStatus");
-        Date creationDate = doc.getDate("creationDate");
-        Date lastUpdatedDate = doc.getDate("lastUpdatedDate");
-        double balance = doc.getDouble("balance");
-        return new Account(balance, accountId, accountHolderName, accountType, accountStatus, creationDate);
     }
 }
--- src/main/java/com/acn3to/core/repositories/TransactionRepository.java
+++ src/main/java/com/acn3to/core/repositories/TransactionRepository.java
@@ -1,18 +1,22 @@
 package com.acn3to.core.repositories;
 
-import com.google.gson.JsonObject;
+import com.acn3to.core.entities.Transaction;
 import com.mongodb.client.MongoCollection;
-import org.bson.Document;
 import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
- * Repository class for storing transactions in a MongoDB collection.
+ * Repository class for managing transactions in a MongoDB collection.
  * <p>
- * This class provides methods for saving transactions to the MongoDB collection.
+ * This class provides methods for saving, finding, and retrieving transactions from the MongoDB collection.
  * </p>
  */
 public class TransactionRepository {
-    private final MongoCollection<Document> collection;
+    private final MongoCollection<Document> transactionsCollection;
 
     /**
      * Constructs a TransactionRepository with a MongoDB connection.
@@ -20,16 +24,65 @@ public class TransactionRepository {
      * @param database the MongoDatabase instance to use
      */
     public TransactionRepository(MongoDatabase database) {
-        this.collection = database.getCollection("transactions");
+        this.transactionsCollection = database.getCollection("transactions");
     }
 
     /**
      * Saves a transaction to the MongoDB collection.
      *
-     * @param transactionJson the transaction details as a JsonObject
+     * @param transaction the Transaction object to save
      */
-    public void save(JsonObject transactionJson) {
-        Document doc = Document.parse(transactionJson.toString());
-        collection.insertOne(doc);
+    public void save(Transaction transaction) {
+        Document document = new Document("accountId", transaction.accountId())
+                .append("date", transaction.date())
+                .append("type", transaction.type())
+                .append("amount", transaction.amount())
+                .append("balanceAfterTransaction", transaction.balanceAfterTransaction());
+        transactionsCollection.insertOne(document);
+    }
+
+    /**
+     * Finds a transaction by its account ID and date.
+     *
+     * @param accountId the ID of the account involved in the transaction
+     * @param date the date and time of the transaction
+     * @return the Transaction object with the given account ID and date, or null if not found
+     */
+    public Transaction findByAccountIdAndDate(int accountId, Date date) {
+        Document query = new Document("accountId", accountId)
+                .append("date", date);
+        Document document = transactionsCollection.find(query).first();
+        if (document == null) {
+            return null;
+        }
+        return new Transaction(
+                document.getInteger("accountId"),
+                document.getDate("date"),
+                document.getString("type"),
+                document.getDouble("amount"),
+                document.getDouble("balanceAfterTransaction")
+        );
+    }
+
+    /**
+     * Finds all transactions for a given account ID.
+     *
+     * @param accountId the ID of the account
+     * @return a list of all Transaction objects for the given account ID
+     */
+    public List<Transaction> findByAccountId(int accountId) {
+        List<Transaction> transactions = new ArrayList<>();
+        Document query = new Document("accountId", accountId);
+        for (Document doc : transactionsCollection.find(query)) {
+            Transaction transaction = new Transaction(
+                    doc.getInteger("accountId"),
+                    doc.getDate("date"),
+                    doc.getString("type"),
+                    doc.getDouble("amount"),
+                    doc.getDouble("balanceAfterTransaction")
+            );
+            transactions.add(transaction);
+        }
+        return transactions;
     }
 }
